ed62ffb28c71bb6156d27695ae31237805c56787
Rene Dohan <dohan.rene@gmail.com>
2022-06-25 11:11:23 -0500

    ...

--- library/src/main/java/renetik/android/store/CSPreferencesStore.kt
+++ library/src/main/java/renetik/android/store/CSPreferencesStore.kt
@@ -9,76 +9,76 @@ import renetik.android.event.owner.CSContext
 import renetik.android.json.*
 import kotlin.reflect.KClass
 
-class CSPreferencesStore(id: String) : CSContext(), renetik.android.store.CSStore {
+class CSPreferencesStore(id: String) : CSContext(), CSStore {
 
-    private val preferences = getSharedPreferences(id, Context.MODE_PRIVATE)
-    override val eventChanged = event<renetik.android.store.CSStore>()
-    override val data: Map<String, Any?> get() = preferences.all
+	private val preferences = getSharedPreferences(id, Context.MODE_PRIVATE)
+	override val eventChanged = event<CSStore>()
+	override val data: Map<String, Any?> get() = preferences.all
 
-    @SuppressLint("CommitPrefEdits")
-    override fun clear() = preferences.edit().clear().apply()
+	@SuppressLint("CommitPrefEdits")
+	override fun clear() = preferences.edit().clear().apply()
 
-    override fun clear(key: String) {
-        val editor = preferences.edit()
-        editor.remove(key)
-        editor.apply()
-    }
+	override fun clear(key: String) {
+		val editor = preferences.edit()
+		editor.remove(key)
+		editor.apply()
+	}
 
-    override fun has(key: String) = preferences.contains(key)
+	override fun has(key: String) = preferences.contains(key)
 
-    override fun set(key: String, value: String?) = value?.let {
-        val editor = preferences.edit()
-        editor.putString(key, it)
-        eventChanged.fire(this@CSPreferencesStore)
-        editor.apply()
-    } ?: clear(key)
+	override fun set(key: String, value: String?) = value?.let {
+		val editor = preferences.edit()
+		editor.putString(key, it)
+		eventChanged.fire(this@CSPreferencesStore)
+		editor.apply()
+	} ?: clear(key)
 
-    override fun get(key: String): String? =
-        catchAllWarnReturnNull { preferences.getString(key, null) }
+	override fun get(key: String): String? =
+		catchAllWarnReturnNull { preferences.getString(key, null) }
 
-    override fun set(key: String, value: Map<String, *>?) =
-        set(key, value?.toJsonString(formatted = isDebug))
+	override fun set(key: String, value: Map<String, *>?) =
+		set(key, value?.toJsonString(formatted = isDebug))
 
-    override fun getMap(key: String): Map<String, *>? =
-        get(key)?.parseJson<Map<String, *>>()
+	override fun getMap(key: String): Map<String, *>? =
+		get(key)?.parseJson<Map<String, *>>()
 
-    // TODO Array can be retrieved and save by using list functions in extensions
-    override fun set(key: String, value: Array<*>?) =
-        set(key, value?.toJsonString(formatted = isDebug))
+	// TODO Array can be retrieved and save by using list functions in extensions
+	override fun set(key: String, value: Array<*>?) =
+		set(key, value?.toJsonString(formatted = isDebug))
 
-    override fun getArray(key: String): Array<*>? =
-        get(key)?.parseJson<List<*>>()?.toTypedArray()
+	override fun getArray(key: String): Array<*>? =
+		get(key)?.parseJson<List<*>>()?.toTypedArray()
 
-    override fun set(key: String, value: List<*>?) =
-        set(key, value?.toJsonString(formatted = isDebug))
+	override fun set(key: String, value: List<*>?) =
+		set(key, value?.toJsonString(formatted = isDebug))
 
-    override fun getList(key: String): List<*>? =
-        get(key)?.parseJson<List<*>>()
+	override fun getList(key: String): List<*>? =
+		get(key)?.parseJson<List<*>>()
 
-    @Suppress("unchecked_cast")
-    override fun <T : renetik.android.json.CSJsonObject> getJsonObjectList(key: String,
-                                                                           type: KClass<T>) =
-        (get(key)?.parseJsonList() as? List<MutableMap<String, Any?>>)
-            ?.let(type::createJsonObjectList)
+	@Suppress("unchecked_cast")
+	override fun <T : CSJsonObject> getJsonObjectList(key: String,
+	                                                  type: KClass<T>) =
+		(get(key)?.parseJsonList() as? List<MutableMap<String, Any?>>)
+			?.let(type::createJsonObjectList)
 
-    override fun <T : renetik.android.json.CSJsonObject> set(key: String, value: T?) =
-        set(key, value?.toJsonString(formatted = isDebug))
+	override fun <T : CSJsonObject> set(key: String, value: T?) =
+		set(key, value?.toJsonString(formatted = isDebug))
 
-    override fun <T : renetik.android.json.CSJsonObject> getJsonObject(key: String,
-                                                                       type: KClass<T>) =
-        get(key)?.parseJsonMap()?.let(type::createJsonObject)
+	override fun <T : CSJsonObject> getJsonObject(key: String,
+	                                              type: KClass<T>) =
+		get(key)?.parseJsonMap()?.let(type::createJsonObject)
 
-    override fun load(store: renetik.android.store.CSStore) = with(preferences.edit()) {
-        loadAll(store)
-        eventChanged.fire(this@CSPreferencesStore)
-        apply()
-    }
+	override fun load(store: CSStore) = with(preferences.edit()) {
+		loadAll(store)
+		eventChanged.fire(this@CSPreferencesStore)
+		apply()
+	}
 
-    override fun reload(store: renetik.android.store.CSStore) = with(preferences.edit()) {
-        clear()
-        loadAll(store)
-        eventChanged.fire(this@CSPreferencesStore)
-        apply()
-    }
+	override fun reload(store: CSStore) = with(preferences.edit()) {
+		clear()
+		loadAll(store)
+		eventChanged.fire(this@CSPreferencesStore)
+		apply()
+	}
 }
 
--- library/src/main/java/renetik/android/store/json/CSStoreJsonObject+.kt
+++ library/src/main/java/renetik/android/store/json/CSStoreJsonObject+.kt
@@ -1,8 +1,9 @@
 package renetik.android.store.json
 
-fun CSStoreJsonObject.getObject(key: String) = getMap(key)?.let(::CSStoreJsonObject)
+fun CSStoreJsonObject.getObject(key: String) =
+	getMap(key)?.let { CSStoreJsonObject().also { load(it) } }
 
 fun CSStoreJsonObject.reload(data: Map<String, Any?>) = bulkSave().use {
-    clear()
-    load(data)
+	clear()
+	load(data)
 }
