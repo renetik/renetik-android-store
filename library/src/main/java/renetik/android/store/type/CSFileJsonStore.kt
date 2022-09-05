14332e5a3cb334e5e3f4f47e4cf331f413b63b3d
Rene Dohan <dohan.rene@gmail.com>
2022-09-05 23:17:27 +0200

    Update CSFileJsonStore.kt

--- library/src/main/java/renetik/android/store/type/CSFileJsonStore.kt
+++ library/src/main/java/renetik/android/store/type/CSFileJsonStore.kt
@@ -7,15 +7,18 @@ import renetik.android.core.lang.CSBackground.background
 import renetik.android.core.lang.CSEnvironment.isDebug
 import java.io.File
 
-class CSFileJsonStore(parent: File, id: String, directory: String = "",
-                      isJsonPretty: Boolean = isDebug)
+class CSFileJsonStore(
+    val file: File, isJsonPretty: Boolean = isDebug)
     : CSJsonStoreBase(isJsonPretty) {
+
+    constructor(parent: File, id: String, directory: String = "",
+                isJsonPretty: Boolean = isDebug)
+            : this(File(File(parent, directory), "$id.json"), isJsonPretty)
 
     constructor(context: Context, id: String, directory: String = "",
                 isJsonPretty: Boolean = false)
             : this(context.filesDir, id, directory, isJsonPretty)
 
-    val file = File(File(parent, directory), "$id.json")
     override fun loadJsonString() = file.readString()
     override fun saveJsonString(json: String) {
         background { file.write(json) }
