c4cf0b925799cc38d7db309d8983d17af817378c
Rene Dohan <dohan.rene@gmail.com>
2023-04-28 12:15:30 +0200

    ...

--- library/src/main/java/renetik/android/store/CSStore.kt
+++ library/src/main/java/renetik/android/store/CSStore.kt
@@ -3,7 +3,6 @@ package renetik.android.store
 import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.core.lang.nullableLazyVar
 import renetik.android.core.logging.CSLog.logWarn
-import renetik.android.core.logging.CSLogMessage.Companion.message
 import renetik.android.event.CSEvent
 import renetik.android.json.obj.CSJsonObjectInterface
 import renetik.android.store.type.CSFileJsonStore
@@ -20,7 +19,7 @@ interface CSStore : Iterable<Map.Entry<String, Any?>>, CSJsonObjectInterface {
     val eventChanged: CSEvent<CSStore>
     val data: Map<String, Any?>
     fun bulkSave(): Closeable =
-        Closeable { logWarn { message("Bulk save not implemented") } }
+        Closeable { logWarn { "Bulk save not implemented" } }
 
     fun load(data: Map<String, Any?>)
     fun reload(data: Map<String, Any?>) = bulkSave().use {
