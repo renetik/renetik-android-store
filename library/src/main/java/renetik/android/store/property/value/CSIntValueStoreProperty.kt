5dc08ff2e657e5912b88b3d4aff280a4b109b59b
Rene Dohan <dohan.rene@gmail.com>
2023-06-15 13:10:47 +0200

    ...

--- library/src/main/java/renetik/android/store/property/value/CSIntValueStoreProperty.kt
+++ library/src/main/java/renetik/android/store/property/value/CSIntValueStoreProperty.kt
@@ -5,8 +5,8 @@ import renetik.android.store.CSStore
 class CSIntValueStoreProperty(
     store: CSStore, key: String,
     override val default: Int,
-    onChange: ((value: Int) -> Unit)? = null)
-    : CSValueStoreProperty<Int>(store, key,  onChange) {
+    onChange: ((value: Int) -> Unit)? = null
+) : CSValueStoreProperty<Int>(store, key, onChange) {
     override fun get(store: CSStore) = store.getInt(key)
     override fun set(store: CSStore, value: Int) = store.set(key, value)
 }
