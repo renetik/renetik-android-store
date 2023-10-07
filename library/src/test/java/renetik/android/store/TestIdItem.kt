455769a949547665a4868ec0bdb3b7b570eff526
Rene Dohan <dohan.rene@gmail.com>
2023-10-07 15:01:13 +0200

    Refactoring entries and register to +.

--- library/src/test/java/renetik/android/store/TestIdItem.kt
+++ library/src/test/java/renetik/android/store/TestIdItem.kt
@@ -6,6 +6,6 @@ enum class TestIdItem(override val id: String) : CSHasId {
     First("id1"), Second("id2"), Third("id3"), Fourth("id4");
 
     companion object {
-        val TestIdItems = values()
+        val TestIdItems = entries
     }
 }
