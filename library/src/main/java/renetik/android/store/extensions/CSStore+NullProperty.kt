c763f84122ed3c974cf25094f69f9c98d974a3b1
Rene Dohan <dohan.rene@gmail.com>
2024-02-09 15:38:55 +0800

    Audio track input model refactoring and improvements

--- library/src/main/java/renetik/android/store/extensions/CSStore+NullProperty.kt
+++ library/src/main/java/renetik/android/store/extensions/CSStore+NullProperty.kt
@@ -1,6 +1,8 @@
 package renetik.android.store.extensions
 
 import renetik.android.core.lang.ArgFunc
+import renetik.android.event.common.CSHasDestruct
+import renetik.android.event.common.registerParent
 import renetik.android.store.CSStore
 import renetik.android.store.property.CSStoreProperty
 import renetik.android.store.property.nullable.CSBooleanNullableStoreProperty
@@ -10,20 +12,30 @@ import renetik.android.store.property.nullable.CSStringNullableStoreProperty
 
 fun CSStore.nullStringProperty(
     key: String, default: String? = null,
-    onChange: ArgFunc<String?>? = null) =
-    CSStringNullableStoreProperty(this, key, default, onChange)
+    onChange: ArgFunc<String?>? = null,
+) = CSStringNullableStoreProperty(this, key, default, onChange)
+
+fun CSStore.nullStringProperty(
+    parent: CSHasDestruct,
+    key: String, default: String? = null,
+    onChange: ArgFunc<String?>? = null,
+) = CSStringNullableStoreProperty(this, key, default, onChange)
+    .registerParent(parent)
 
 fun CSStore.nullBoolProperty(
     key: String, default: Boolean? = null,
-    onChange: ArgFunc<Boolean?>? = null): CSStoreProperty<Boolean?> =
+    onChange: ArgFunc<Boolean?>? = null,
+): CSStoreProperty<Boolean?> =
     CSBooleanNullableStoreProperty(this, key, default, onChange)
 
 fun CSStore.nullIntProperty(
     key: String, default: Int? = null,
-    onChange: ArgFunc<Int?>? = null): CSStoreProperty<Int?> =
+    onChange: ArgFunc<Int?>? = null,
+): CSStoreProperty<Int?> =
     CSIntNullableStoreProperty(this, key, default, onChange)
 
 fun CSStore.nullFloatProperty(
     key: String, default: Float? = null,
-    onChange: ArgFunc<Float?>? = null): CSStoreProperty<Float?> =
+    onChange: ArgFunc<Float?>? = null,
+): CSStoreProperty<Float?> =
     CSFloatNullableStoreProperty(this, key, default, onChange)
