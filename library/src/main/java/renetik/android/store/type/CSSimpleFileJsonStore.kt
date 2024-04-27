8a027c5a018b8ad1b0096388d7ccb0c4346ad563
Rene Dohan <dohan.rene@email.com>
2024-04-27 17:04:42 +0800

    ...

--- library/src/main/java/renetik/android/store/type/CSSimpleFileJsonStore.kt
+++ library/src/main/java/renetik/android/store/type/CSSimpleFileJsonStore.kt
@@ -6,18 +6,17 @@ import renetik.android.core.kotlin.changeIf
 import renetik.android.core.lang.CSEnvironment
 import renetik.android.json.parseJsonMap
 import renetik.android.json.toJson
-import renetik.android.store.type.CSJsonObjectStore
 import java.io.File
 
 class CSSimpleFileJsonStore(
     val file: File,
-    private val isPretty: Boolean = CSEnvironment.isDebug
+    private val isJsonPretty: Boolean = CSEnvironment.isDebug
 ) : CSJsonObjectStore() {
 
     override val data: MutableMap<String, Any?> by lazy {
         file.readString()?.parseJsonMap() ?: mutableMapOf()
     }
 
-    fun save() = file.write(data.changeIf(isPretty) { it.toSortedMap() }
-        .toJson(formatted = isPretty))
+    fun save() = file.write(data.changeIf(isJsonPretty) { it.toSortedMap() }
+        .toJson(formatted = isJsonPretty))
 }
