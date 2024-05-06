3fe36487d351712655983d403424ae13cabeb307
Rene Dohan <dohan.rene@email.com>
2024-05-06 20:57:12 +0800

    ...

--- library/src/main/java/renetik/android/store/type/CSJsonObjectStore.kt
+++ library/src/main/java/renetik/android/store/type/CSJsonObjectStore.kt
@@ -7,8 +7,10 @@ import renetik.android.store.CSStore
 open class CSJsonObjectStore : CSJsonObject(), CSStore {
     override val eventLoaded = event<CSStore>()
     override val eventChanged = event<CSStore>()
+    override fun load(data: Map<String, Any?>) = super.load(data)
     override fun onLoaded() = eventLoaded.fire(this)
     open fun onChanged() = eventChanged.fire(this)
+
     override fun onChange() {
         if (!isBulkSave) onChanged()
         else isBulkSaveDirty = true
