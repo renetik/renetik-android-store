d39338e92ab897d5b880e32054f5c0363b646301
Rene Dohan <dohan.rene@email.com>
2024-05-20 23:29:34 +0800

    ...

--- library/src/main/java/renetik/android/store/property/CSStoreProperty+.kt
+++ library/src/main/java/renetik/android/store/property/CSStoreProperty+.kt
@@ -4,4 +4,3 @@ import renetik.android.store.CSStore
 
 fun <T> CSStoreProperty<T>.saveTo(store: CSStore) = set(store, value)
 fun <T> CSStoreProperty<T>.save() = set(store, value)
-//val CSStoreProperty<*>.isSaved get() = store.has(key)
