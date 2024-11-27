5d31aac81be02e5a00af6cb0778be80bab65b980
Rene Dohan <dohan.rene@email.com>
2024-11-27 16:24:02 +0800

    ...

--- library/src/main/java/renetik/android/store/context/CSHasStoreContext.kt
+++ library/src/main/java/renetik/android/store/context/CSHasStoreContext.kt
@@ -1,10 +1,12 @@
 package renetik.android.store.context
 
+import renetik.android.core.lang.CSHasId
 import renetik.android.event.common.CSHasDestruct
 import renetik.android.event.common.destruct
 
-interface CSHasStoreContext {
+interface CSHasStoreContext: CSHasId {
     val store: StoreContext
+    override val id get() = store.id
 
     companion object {
         fun <T> T.clearDestruct() where T : CSHasStoreContext, T : CSHasDestruct {
