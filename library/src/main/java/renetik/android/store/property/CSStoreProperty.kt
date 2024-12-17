d7eb0d4ee7c308b9fbf446afe4004483b4d27f26
Rene Dohan <dohan.rene@email.com>
2024-12-17 21:06:01 +0800

    ...

--- library/src/main/java/renetik/android/store/property/CSStoreProperty.kt
+++ library/src/main/java/renetik/android/store/property/CSStoreProperty.kt
@@ -9,8 +9,19 @@ interface CSStoreProperty<T> : CSProperty<T> {
     val store: CSStore
     val key: String
     fun set(store: CSStore, value: T)
+
+    @Deprecated("If <T : CSStoreProperty<Int>> T.max(value: Int) " +
+            "and others works this is basecally unused and makes " +
+            "things significantly more complex in multiple places",
+        ReplaceWith("Remove"))
     var filter: ((T?) -> T?)?
+
+    @Deprecated("If <T : CSStoreProperty<Int>> T.max(value: Int) " +
+            "and others works this is basecally unused and makes " +
+            "things significantly more complex in multiple places",
+        ReplaceWith("Remove"))
     fun getFiltered(store: CSStore): T?
+
     val isSaved get() = store.has(key)
     fun listenStoreLoad()
     fun trackModified(track: Boolean = true): CSStoreProperty<T> = this
