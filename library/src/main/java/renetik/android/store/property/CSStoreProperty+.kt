08c991d9f1c77b5584829b7b60c46dfc96847366
Rene Dohan <dohan.rene@gmail.com>
2025-04-28 12:19:47 +0800

    Add save overload for CSStoreProperty to allow explicit value.

--- library/src/main/java/renetik/android/store/property/CSStoreProperty+.kt
+++ library/src/main/java/renetik/android/store/property/CSStoreProperty+.kt
@@ -4,3 +4,4 @@ import renetik.android.store.CSStore
 
 fun <T> CSStoreProperty<T>.saveTo(store: CSStore) = set(store, value)
 fun <T> CSStoreProperty<T>.save() = set(store, value)
+fun <T> CSStoreProperty<T>.save(value: T) = set(store, value)
