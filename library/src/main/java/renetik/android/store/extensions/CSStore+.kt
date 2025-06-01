898fc36bef8c63d27a928fef747e5fd7fd277086
Rene Dohan <dohan.rene@gmail.com>
2025-06-01 22:59:44 +0800

    Add isEmpty property to CSStore
    This commit adds an `isEmpty` property to the `CSStore` class. This property returns `true` if the `data` map of the store is empty, and `false` otherwise. This allows for a more concise way to check if a store has any data.

--- library/src/main/java/renetik/android/store/extensions/CSStore+.kt
+++ library/src/main/java/renetik/android/store/extensions/CSStore+.kt
@@ -6,6 +6,7 @@ import renetik.android.store.CSStore
 fun <T : CSStore> T.load(store: CSStore) = apply { load(store.data) }
 fun <T : CSStore> T.reload(store: CSStore) = apply { reload(store.data) }
 fun <T : CSStore> T.reload(json: String) = apply { reload(json.parseJsonMap()!!) }
+val <T : CSStore> T.isEmpty get() = data.isEmpty()
 
 inline fun <T : CSStore, R> T.operation(func: (T) -> R) {
     if (!startOperation()) {
