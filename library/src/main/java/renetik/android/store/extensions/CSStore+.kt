73fae0df833d109df60367c4620137d7168a29fc
Rene Dohan <dohan.rene@gmail.com>
2025-08-08 13:07:23 +0800

    feat: Add load from CSStore by key
    This commit introduces a new `load` extension function for `CSStore` that allows loading data from another `CSStore` instance using a specific key.
    
    - Added an inline extension function `load(store: CSStore, key: String)` to `CSStore`.
    - This function retrieves the data associated with the given `key` from the source `store`.
    - If the retrieved data is a `Map<String, Any?>`, it is loaded into the current store.
    - An error is logged if the data associated with the key is not a `Map`.
    - The existing `load(store: CSStore)` function remains unchanged and loads the entire data from the source store.
    - Added `@file:Suppress("NOTHING_TO_INLINE")` to the file.

--- library/src/main/java/renetik/android/store/extensions/CSStore+.kt
+++ library/src/main/java/renetik/android/store/extensions/CSStore+.kt
@@ -1,13 +1,24 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
 package renetik.android.store.extensions
 
+import renetik.android.core.logging.CSLog.logError
 import renetik.android.json.parseJsonMap
 import renetik.android.store.CSStore
 
-fun <T : CSStore> T.load(store: CSStore) = apply { load(store.data) }
-fun <T : CSStore> T.reload(store: CSStore) = apply { reload(store.data) }
-fun <T : CSStore> T.reload(json: String) = apply { reload(json.parseJsonMap()!!) }
-val <T : CSStore> T.isEmpty get() = data.isEmpty()
-val <T : CSStore> T.isNotEmpty get() = data.isNotEmpty()
+inline fun <T : CSStore> T.load(store: CSStore, key: String) = apply {
+    store.data[key]?.also {
+        @Suppress("UNCHECKED_CAST")
+        (it as? Map<String, Any?>)?.also(::load)
+            ?: logError("Key is not store Map: $key")
+    }
+}
+
+inline fun <T : CSStore> T.load(store: CSStore) = apply { load(store.data) }
+inline fun <T : CSStore> T.reload(store: CSStore) = apply { reload(store.data) }
+inline fun <T : CSStore> T.reload(json: String) = apply { reload(json.parseJsonMap()!!) }
+inline val <T : CSStore> T.isEmpty get() = data.isEmpty()
+inline val <T : CSStore> T.isNotEmpty get() = data.isNotEmpty()
 
 inline fun <T : CSStore, R> T.operation(func: (T) -> R) {
     if (!startOperation()) {
