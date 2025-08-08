feb0a1da53423c876f6a155fe5a202adf01da99b
Rene Dohan <dohan.rene@gmail.com>
2025-08-08 16:24:51 +0800

    Refactor: Use `localizedPresetTitle` for preset item titles
    This commit replaces the usage of `getStringPresetItem` with the more appropriately named `localizedPresetTitle` function in `InstApplication.kt`.
    
    The following files have been updated to use the new function name:
    - `PresetItemView.kt`
    - `MetronomeView.kt`
    - `SequenceEditorUnselectedToolbar.kt`
    - `MetronomeAppView.kt`
    
    Additionally, a TODO comment was added in `CSIntNullableStoreProperty.kt` to move `CSDoubleNullableStoreProperty` to a separate file.

--- library/src/main/java/renetik/android/store/property/nullable/CSIntNullableStoreProperty.kt
+++ library/src/main/java/renetik/android/store/property/nullable/CSIntNullableStoreProperty.kt
@@ -14,6 +14,7 @@ class CSIntNullableStoreProperty(
 }
 
 
+// TODO mode to separate file
 class CSDoubleNullableStoreProperty(
     store: CSStore, key: String,
     override val default: Double? = null,
