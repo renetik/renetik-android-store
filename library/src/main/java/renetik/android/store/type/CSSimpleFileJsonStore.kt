b8d864c2faca6964c50c151d42c3abf6917156e9
Rene Dohan <dohan.rene@gmail.com>
2025-12-20 02:01:36 +0800

    refactor: Make `CSSimpleFileJsonStore.save` a suspend function
    This commit refactors the `CSSimpleFileJsonStore.save` method to be a `suspend` function. The file writing operation is now explicitly performed on the IO dispatcher using `app.IO`, making the operation asynchronous and non-blocking.

--- library/src/main/java/renetik/android/store/type/CSSimpleFileJsonStore.kt
+++ library/src/main/java/renetik/android/store/type/CSSimpleFileJsonStore.kt
@@ -5,6 +5,7 @@ import renetik.android.core.java.io.readString
 import renetik.android.core.java.io.write
 import renetik.android.core.kotlin.changeIf
 import renetik.android.core.lang.CSEnvironment
+import renetik.android.core.lang.result.invoke
 import renetik.android.json.CSJson
 import renetik.android.json.parseJsonMap
 import renetik.android.json.toJson
@@ -28,6 +29,8 @@ class CSSimpleFileJsonStore(
     override val data: MutableMap<String, Any?> =
         file.readString()?.parseJsonMap() ?: mutableMapOf()
 
-    fun save() = file.write(data.changeIf(isJsonPretty) { toSortedMap() }
-        .toJson(formatted = isJsonPretty))
+    suspend fun save() = app.IO {
+        file.write(data.changeIf(isJsonPretty) { toSortedMap() }
+            .toJson(formatted = isJsonPretty))
+    }
 }
